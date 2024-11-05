457eb42f784bb4bb0319e7db6a67b61834a50280
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-11-05 18:48:18 +0500

    Update cache

--- js/app.js
+++ js/app.js
@@ -1,10 +1,1 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/service-worker.js")
-    .then(function () {
-      console.log("Service Worker muvaffaqiyatli registratsiya qilindi!");
-    })
-    .catch(function (error) {
-      console.log("Service Worker registratsiyasi muvaffaqiyatsiz:", error);
-    });
-}
+console.log("Ishladi oxiri");
