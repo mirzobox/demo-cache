fcabb99c498d57f3d7a75eb954ea9aa5d6aad6c4
mirzobox <mirzoulugbekkhudoyberdiyev@gmail.com>
2024-11-05 18:41:12 +0500

    Initial commit

---
+++ js/service-worker.js
@@ -0,0 +1,23 @@
+// Kesh nomi va keshlashni istagan resurslar ro'yxati
+const CACHE_NAME = "my-cache-v1";
+const urlsToCache = ["/", "/index.html", "/css/style.css", "/js/app.js"];
+
+// 'install' hodisasi - resurslarni keshga yuklaymiz
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Resurslar keshlanyapti");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 'fetch' hodisasi - resurslarni keshdan qaytarish
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Keshdagi ma'lumotni qaytarish yoki tarmoqdan olish
+      return response || fetch(event.request);
+    })
+  );
+});
